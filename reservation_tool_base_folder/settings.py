70ea85387bd0c492bca26a46756b4cd4c6192ed8
gabrymattioli <gabrymattioli@gmail.com>
2021-09-30 15:06:01 +0200

    Fixed secret key

--- reservation_tool_base_folder/settings.py
+++ reservation_tool_base_folder/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open("secret_key.txt") as f:
+with open("/etc/reservation_tool_secret_key.txt") as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
