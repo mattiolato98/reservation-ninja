620b21c7e1c42d26b0d9e0680fd762f201dec4dc
gabrymattioli <gabrymattioli@gmail.com>
2021-09-30 16:05:35 +0200

    Added login required mixin to homepage view

--- reservation_tool_base_folder/views.py
+++ reservation_tool_base_folder/views.py
@@ -2,5 +2,5 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
-class HomepageView(TemplateView):
+class HomepageView(LoginRequiredMixin, TemplateView):
     template_name = 'home.html'
