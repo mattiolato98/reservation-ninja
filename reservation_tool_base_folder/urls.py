ac83107eaad9a721372d7117a412d455ae1bcad2
gabrymattioli <gabrymattioli@gmail.com>
2021-09-30 15:26:31 +0200

    Added homepage url

--- reservation_tool_base_folder/urls.py
+++ reservation_tool_base_folder/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from reservation_tool_base_folder.views import HomepageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomepageView.as_view(), name='home'),
 ]
