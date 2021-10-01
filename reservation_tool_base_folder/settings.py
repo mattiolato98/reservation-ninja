086c765c6ae871701089d9f2f904ad84d307f4d2
Filippo Fontana <238290@studenti.unimore.it>
2021-10-01 15:52:34 +0200

    Added random_secret_key() to settings.py

--- reservation_tool_base_folder/settings.py
+++ reservation_tool_base_folder/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open("/etc/reservation_tool_secret_key.txt") as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
