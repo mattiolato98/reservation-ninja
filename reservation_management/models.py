65fdddfe7fadff6c38500ab14984f4a1a5588b9a
gabrymattioli <gabrymattioli@gmail.com>
2021-10-01 11:39:29 +0200

    Added Building and Classroom models

--- reservation_management/models.py
+++ reservation_management/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Building(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Classroom(models.Model):
+    name = models.CharField(max_length=10)
+    building = models.ForeignKey(Building, on_delete=models.CASCADE, related_name='classrooms')
+
+    def __str__(self):
+        return self.name
