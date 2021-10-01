a9c345f397c67b4e07e31e7d63d377848bbe5f25
gabrymattioli <gabrymattioli@gmail.com>
2021-10-01 11:39:59 +0200

    Added Unimore credentials to user model and corresponding form

--- user_management/forms.py
+++ user_management/forms.py
@@ -17,6 +17,8 @@ class LoginForm(AuthenticationForm):
 class PlatformUserCreationForm(UserCreationForm):
     helper = FormHelper()
     email = forms.EmailField(required=True)
+    unimore_username = forms.CharField(required=True)
+    unimore_password = forms.CharField(required=True)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -26,7 +28,9 @@ class PlatformUserCreationForm(UserCreationForm):
         self.fields['username'].widget.attrs.update({'placeholder': 'Username'})
         self.fields['email'].widget.attrs.update({'placeholder': 'Email'})
         self.fields['password1'].widget.attrs.update({'placeholder': 'Password'})
-        self.fields['password2'].widget.attrs.update({'placeholder': 'Ripeti la password'})
+        self.fields['password2'].widget.attrs.update({'placeholder': 'Confirm password'})
+        self.fields['unimore_username'].widget.attrs.update({'placeholder': 'Unimore username'})
+        self.fields['unimore_password'].widget.attrs.update({'placeholder': 'Unimore password'})
 
     class Meta:
         model = get_user_model()
@@ -36,3 +40,6 @@ class PlatformUserCreationForm(UserCreationForm):
             'password1',
             'password2',
         ]
+        widgets = {
+            'unimore_password': forms.PasswordInput(),
+        }
--- user_management/models.py
+++ user_management/models.py
@@ -6,5 +6,8 @@ class PlatformUser(AbstractUser):
     AbstractUser._meta.get_field('email')._unique = True
     is_manager = models.BooleanField(default=False)
 
+    unimore_username = models.CharField(max_length=100)
+    unimore_password = models.CharField(max_length=100)
+
     def __str__(self):
         return self.username
