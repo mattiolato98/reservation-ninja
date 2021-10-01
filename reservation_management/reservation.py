64234a11b1c14faf230536336b6f75707d4dddb3
gabrymattioli <gabrymattioli@gmail.com>
2021-10-01 21:22:23 +0200

    Fixed xpaths and refactored

--- reservation_management/reservation.py
+++ reservation_management/reservation.py
@@ -29,8 +29,6 @@ def find_hours(root_element, start_hour, end_hour):
 
 
 def reserve_room(driver, start_time, end_time, building, room):
-    driver.get("https://www.unimore.it/covid19/trovaaula.html")
-
     if driver.find_element_by_id("cookie-bar"):
         driver.find_element_by_xpath(
             "//*[@id='cookie-bar']//a[contains(text(), 'OK')]"
@@ -38,13 +36,13 @@ def reserve_room(driver, start_time, end_time, building, room):
         t.sleep(1)
 
     driver.find_element_by_xpath(
-        f"//li[contains(text(), 'Edificio {building}')]"
+        f"//li[contains(text(), '{building}')]"
         "//a[contains(text(), 'Elenco Aule con link per registrazione presenza')]"
     ).click()
     t.sleep(1)
 
     room_element = driver.find_element_by_xpath(
-        f"//td[contains(text(), 'Aula {room}')]/ancestor::tr"
+        f"//td[contains(text(), '{room}')]/ancestor::tr"
     )
     
     ranges = find_hours(room_element, start_time, end_time)
@@ -52,7 +50,7 @@ def reserve_room(driver, start_time, end_time, building, room):
     building_url = driver.current_url
     for range_start_time, range_end_time in ranges:
         driver.find_element_by_xpath(
-            f"//td[contains(text(), 'Aula {room}')]"
+            f"//td[contains(text(), '{room}')]"
             f"/ancestor::tr//a[contains(text(), 'Turno Aula {range_start_time}-{range_end_time}')]"
         ).click()
         t.sleep(1)
@@ -76,21 +74,17 @@ def reserve_room(driver, start_time, end_time, building, room):
         # driver.close()
 
 
-if __name__ == "__main__":
+def automatic_reservation():
     driver = webdriver.Firefox()
 
     for user in get_user_model().objects.exclude(pk=1):
-        lessons = user.today_lessons()
-    # lessons = [
-    #     {
-    #         "start_time": time(9),
-    #         "end_time": time(12),
-    #         "building": "Fisica",
-    #         "room": "L1.2"
-    #     },
-    # ]
-
-        for lesson in lessons:
+        print(f"UTENTE {user.username} -----------------------------------------------------------------------------")
+        for lesson in user.today_lessons():
+            print(
+                f"Prenotando {lesson.classroom.building.name} {lesson.classroom.name} - "
+                f"{lesson.start_time}/{lesson.end_time}"
+            )
+            driver.get("https://www.unimore.it/covid19/trovaaula.html")
             reserve_room(
                 driver,
                 lesson.start_time,
@@ -98,3 +92,9 @@ if __name__ == "__main__":
                 lesson.classroom.building.name,
                 lesson.classroom.name
             )
+
+        driver.delete_all_cookies()
+
+
+if __name__ == "__main__":
+    automatic_reservation()
