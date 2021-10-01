a2dcb94c4ea9c935ca384ac7ea49c0bc7a36a95b
gabrymattioli <gabrymattioli@gmail.com>
2021-10-01 12:11:50 +0200

    Added Timetable model to admin

--- reservation_management/admin.py
+++ reservation_management/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from reservation_management.models import Classroom, Building
+from reservation_management.models import Classroom, Building, Timetable
 
 admin.site.register(Building)
 admin.site.register(Classroom)
+admin.site.register(Timetable)
