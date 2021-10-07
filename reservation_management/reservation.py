1a6137f3354e4eeb0178c6f43b5794a1648430bd
gabrymattioli <gabrymattioli@gmail.com>
2021-10-07 13:42:46 +0200

    Updated reservation.py

--- reservation_management/reservation.py
+++ reservation_management/reservation.py
@@ -3,18 +3,16 @@ from os.path import dirname
 import django
 import os
 import sys
-# import time as t
-from time import time as measure_time
+# from time import time as measure_time
+from datetime import datetime
 
 from datetime import time
 
+import pytz
 from django.contrib.auth import get_user_model
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 
 PROJECT_PATH = os.path.join(dirname(__file__), "../")
 RESERVATION_URL = 'https://www.unimore.it/covid19/trovaaula.html'
@@ -37,33 +35,11 @@ def find_hours(root_element, start_hour, end_hour):
         )[-1],
         available_hours
     ))
-    # t.sleep(1)
 
     return ranges_to_reserve
 
 
 def reserve_room(driver, user, start_time, end_time, building, room):
-    # TODO: FIRST SOLUTION (WITH SLEEPS)
-    # if driver.find_element_by_id("cookie-bar"):
-    #     driver.find_element_by_xpath(
-    #         "//*[@id='cookie-bar']//a[contains(text(), 'OK')]"
-    #     ).click()
-    #     t.sleep(1)
-
-    # driver.find_element_by_xpath(
-    #     f"//li[contains(text(), '{building}')]"
-    #     "//a[contains(text(), 'Elenco Aule con link per registrazione presenza')]"
-    # ).click()
-    # t.sleep(1)
-    # TODO: SECOND SOLUTION, MAYBE USEFUL FOR FUTURE TASKS, BUT NOT IN THIS CASE...
-    # WebDriverWait(driver, TIME_INTERVAL).until(EC.visibility_of_element_located(
-    #     (
-    #         By.XPATH,
-    #         f"//li[contains(text(), '{building}')]"
-    #         "//a[contains(text(), 'Elenco Aule con link per registrazione presenza')]"
-    #     )
-    # )).click()
-    # TODO: THIRD SOLUTION, IT WORKS WITHOUT THE FIRST IF, PERFORMANCE TEST NEEDED AGAINST THE FIRST SOLUTION
     element = driver.find_element_by_xpath(
         f"//li[contains(text(), '{building}')]"
         "//a[contains(text(), 'Elenco Aule con link per registrazione presenza')]"
@@ -73,16 +49,17 @@ def reserve_room(driver, user, start_time, end_time, building, room):
     room_element = driver.find_element_by_xpath(
         f"//td[contains(text(), '{room}')]/ancestor::tr"
     )
-    
+
     ranges = find_hours(room_element, start_time, end_time)
-    
+
     building_url = driver.current_url
+    # TODO: sembra che questo ciclo faccia un'iterazione in più!
     for range_start_time, range_end_time in ranges:
-        driver.find_element_by_xpath(
+        element = driver.find_element_by_xpath(
             f"//td[contains(text(), '{room}')]"
             f"/ancestor::tr//a[contains(text(), 'Turno Aula {range_start_time}-{range_end_time}')]"
-        ).click()
-        # t.sleep(1)
+        )
+        driver.execute_script("arguments[0].click();", element)
 
         try:
             driver.find_element_by_id("username").send_keys(user.unimore_username)
@@ -94,51 +71,65 @@ def reserve_room(driver, user, start_time, end_time, building, room):
             pass
 
         button = driver.find_element_by_xpath("//button[contains(text(), 'Inserisci')]")
-        print(f"{button.text} presenza {range_start_time}-{range_end_time}")
+        button.click()
+
+        print(f"Presenza inserita {range_start_time}-{range_end_time}")
+
         driver.get(building_url)
         # t.sleep(1)
 
-        # button.click()
 
-
-def automatic_reservation():
-    driver = webdriver.Firefox()
-    # Selenium configuration:
-    driver.implicitly_wait(TIME_INTERVAL)
-    #driver.maximize_window()
-
-    start_time = measure_time()
-    for user in get_user_model().objects.exclude(enable_automatic_reservation=False):
-        print(f"UTENTE {user.username} -----------------------------------------------------------------------------")
-        for lesson in user.today_lessons:
-            print(
-                f"Prenotando {lesson.classroom.building.name} {lesson.classroom.name} - "
-                f"{lesson.start_time}/{lesson.end_time}"
-            )
-            driver.get(RESERVATION_URL)
-            reserve_room(
-                driver,
-                user,
-                lesson.start_time,
-                lesson.end_time,
-                lesson.classroom.building.name,
-                lesson.classroom.name
-            )
-
-        driver.delete_all_cookies()
-
-    driver.close()
-    end_time = measure_time()
-    print(f"Elapsed time: {end_time - start_time}")
+# TODO: bisongerebbe darle un'ultima speranza con più utenti...
+# def automatic_reservation():
+#     driver = webdriver.Firefox()
+#     # Selenium configuration:
+#     driver.implicitly_wait(TIME_INTERVAL)
+#     # driver.maximize_window()
+#
+#     # for user in get_user_model().objects.exclude(enable_automatic_reservation=False):
+#     for user in get_user_model().objects.filter(username="mattiolato"):
+#         print(f"UTENTE {user.username} -----------------------------------------------------------------------------")
+#         for lesson in user.today_lessons:
+#             print(
+#                 f"Prenotando {lesson.classroom.building.name} {lesson.classroom.name} - "
+#                 f"{lesson.start_time}/{lesson.end_time}"
+#             )
+#             driver.get(RESERVATION_URL)
+#             reserve_room(
+#                 driver,
+#                 user,
+#                 lesson.start_time,
+#                 lesson.end_time,
+#                 lesson.classroom.building.name,
+#                 lesson.classroom.name
+#             )
+#
+#         driver.delete_all_cookies()
+#
+#     driver.close()
 
 
 def reserve_lesson_map(lesson):
-    driver = webdriver.Firefox()
+    # Allows run Firefox on a system with no display
+    options = Options()
+    options.headless = True
+
+    driver = webdriver.Firefox(options=options)
+
     # Selenium configuration:
     driver.implicitly_wait(TIME_INTERVAL)
-    # driver.maximize_window()
 
-    print(f'Prenotando: {lesson}')
+    print(f'Reserving: {lesson}')
+    """
+    l'ideale è creare ad esempio 3 tab e ciclare iterativamente su di essi con l'operatore modulo, in questo modo
+    dovremmo ottimizzare al massimo il driver e l'occupazione di memoria e cpu.
+
+    ogni map calcola che tab utilizzare (mediante un contatore globale + operatore modulo(3)) e fa quello che deve 
+    su quel tab.
+
+    Il driver a quel punto viene creato direttamente dal main così può essere chiuso da lì.
+    """
+    # driver.execute_script(f"window.open('{RESERVATION_URL}', '_blank');")
     driver.get(RESERVATION_URL)
     reserve_room(
         driver,
@@ -148,6 +139,8 @@ def reserve_lesson_map(lesson):
         lesson.classroom.building.name,
         lesson.classroom.name
     )
+    driver.delete_all_cookies()
+    driver.close()
 
 
 if __name__ == "__main__":
@@ -157,11 +150,13 @@ if __name__ == "__main__":
     django.setup()
 
     # automatic_reservation()
+
     from reservation_management.models import Lesson
 
-    lessons = Lesson.objects.filter(day=3).order_by('start_time')
-    start = measure_time()
+    lessons = Lesson.objects.filter(day=datetime.now(pytz.timezone('Europe/Rome')).weekday())
+
+    # start = measure_time()
     # TODO: understand if this assignment is required...
-    a = list(map(reserve_lesson_map, lessons))
-    end = measure_time()
-    print(f'{end - start}')
+    dummy_var = list(map(reserve_lesson_map, lessons))
+    # end = measure_time()
+    # print(f'{end - start}')
