384e29d422b4a21a6c4b178fd09ccd955c0746ed
gabrymattioli <gabrymattioli@gmail.com>
2021-10-12 00:08:13 +0200

    Added translation support

--- reservation_tool_base_folder/settings.py
+++ reservation_tool_base_folder/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',  # new
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
