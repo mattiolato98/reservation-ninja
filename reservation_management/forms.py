cc07f9369a6e641e13555c419a5bee27c4c814cd
Filippo Fontana <filippofontana1998@gmail.com>
2021-10-12 23:46:26 +0200

    Created reservation_management/forms.py

---
+++ reservation_management/forms.py
@@ -0,0 +1,44 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Row, Column, HTML
+from django.utils.translation import gettext_lazy as _
+from django import forms
+
+from reservation_management.models import Lesson
+
+"""
+per i giorni della settimana fornire in input una lista, lato server tradurre i giorni in numero.
+ottenere questo con un dizionario, ad ogni giorno della settimana (chiave), è associato il corrispettivo indice 
+numerico (valore) 
+"""
+
+
+class LessonForm(forms.ModelForm):
+    helper = FormHelper()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper.layout = Layout(
+            Row(
+                Column('day', css_class='form-group mb-0'),
+                css_class='form-row'
+            ),
+            Row(
+                Column('start_time', css_class='form-group'),
+                Column('end_time', css_class='form-group'),
+                css_class='form-row',
+            ),
+            Row(
+                Column('classroom', css_class='form-group mb-0'),
+                css_class='form-row'
+            ),
+        )
+
+    class Meta:
+        model = Lesson
+        fields = (
+            "day",
+            "start_time",
+            "end_time",
+            "classroom",
+        )
