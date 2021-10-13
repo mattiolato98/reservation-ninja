b35153acfc2fb662297e2e0886bd277565b625b6
gabrymattioli <gabrymattioli@gmail.com>
2021-10-13 14:21:07 +0200

    Added methods to encrypt/decrypt unimore_password

--- user_management/models.py
+++ user_management/models.py
@@ -1,3 +1,5 @@
+from cryptography.fernet import Fernet
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from datetime import datetime
@@ -15,6 +17,16 @@ class PlatformUser(AbstractUser):
     def __str__(self):
         return self.username
 
+    def save(self, *args, **kwargs):
+        encryptor = Fernet(settings.CRYPTOGRAPHY_KEY.encode())
+        self.unimore_password = encryptor.encrypt(self.unimore_password.encode()).decode()
+        super(PlatformUser, self).save(*args, **kwargs)
+
     @property
     def today_lessons(self):
         return self.lessons.filter(day=datetime.today().weekday())
+
+    @property
+    def clear_unimore_password(self):
+        decryptor = Fernet(settings.CRYPTOGRAPHY_KEY.encode())
+        return decryptor.decrypt(self.unimore_password.encode()).decode()
