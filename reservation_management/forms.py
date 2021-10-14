54055fda404fba4fa169be63194eef2e85f8af55
gabrymattioli <gabrymattioli@gmail.com>
2021-10-14 14:57:03 +0200

    Added weekday choice field

--- reservation_management/forms.py
+++ reservation_management/forms.py
@@ -15,12 +15,28 @@ numerico (valore)
 class LessonForm(forms.ModelForm):
     helper = FormHelper()
 
+    weekday = forms.ChoiceField(choices=[
+        (0, _("Monday")),
+        (1, _("Tuesday")),
+        (2, _("Wednesday")),
+        (3, _("Thursday")),
+        (4, _("Friday")),
+    ],
+        widget=forms.Select(
+            attrs={
+                'class': 'selectpicker',
+                'data-live-search': 'true',
+                'data-dropdown-align-right': 'true'
+            }
+        )
+    )
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.helper.layout = Layout(
             Row(
-                Column('day', css_class='form-group mb-0'),
+                Column('weekday', css_class='form-group mb-0'),
                 css_class='form-row'
             ),
             Row(
