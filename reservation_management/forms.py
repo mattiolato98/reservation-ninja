84fccc5b2cfefea82d8989a9d290dec84a884a49
Filippo Fontana <filippofontana1998@gmail.com>
2021-10-15 12:04:01 +0200

    Fixed how day field is showed

--- reservation_management/forms.py
+++ reservation_management/forms.py
@@ -15,28 +15,12 @@ numerico (valore)
 class LessonForm(forms.ModelForm):
     helper = FormHelper()
 
-    weekday = forms.ChoiceField(choices=[
-        (0, _("Monday")),
-        (1, _("Tuesday")),
-        (2, _("Wednesday")),
-        (3, _("Thursday")),
-        (4, _("Friday")),
-    ],
-        widget=forms.Select(
-            attrs={
-                'class': 'selectpicker',
-                'data-live-search': 'true',
-                'data-dropdown-align-right': 'true'
-            }
-        )
-    )
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.helper.layout = Layout(
             Row(
-                Column('weekday', css_class='form-group mb-0'),
+                Column('day', css_class='form-group mb-0'),
                 css_class='form-row'
             ),
             Row(
@@ -59,6 +43,7 @@ class LessonForm(forms.ModelForm):
             "classroom",
         )
         widgets = {
+            'day': forms.Select(attrs={'class': 'selectpicker', 'data-live-search': 'true'}),
             'classroom': forms.Select(attrs={'class': 'selectpicker', 'data-live-search': 'true'}),
             'start_time': forms.TimeInput(attrs={'type': 'time'}),
             'end_time': forms.TimeInput(attrs={'type': 'time'}),
