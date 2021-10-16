ef1aab789b02e0a5abd450264eb209b758dc39d2
Filippo Fontana <filippofontana1998@gmail.com>
2021-10-16 12:03:29 +0200

    Added reservation-list url

--- reservation_management/urls.py
+++ reservation_management/urls.py
@@ -4,6 +4,7 @@ from reservation_management import views
 app_name = 'reservation_management'
 
 urlpatterns = [
+    path('', views.ReservationListView.as_view(), name='reservation-list'),
     path('lesson/add', views.LessonAddView.as_view(), name='lesson-add'),
     path('lesson/list', views.LessonListView.as_view(), name='lesson-list'),
 ]
