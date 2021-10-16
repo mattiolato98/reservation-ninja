77fc911d9a7730cc045ff2487062bd7279a1eab1
gabrymattioli <gabrymattioli@gmail.com>
2021-10-17 01:06:09 +0200

    Added Reservation model to admin

--- reservation_management/admin.py
+++ reservation_management/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from reservation_management.models import Classroom, Building, Lesson
+from reservation_management.models import Classroom, Building, Lesson, Reservation
 
 admin.site.register(Building)
 admin.site.register(Classroom)
 admin.site.register(Lesson)
+admin.site.register(Reservation)
