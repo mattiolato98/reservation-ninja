b4f4fdc09782a5e87bb8e96d2cc691cff5e608a9
gabrymattioli <gabrymattioli@gmail.com>
2021-10-16 15:28:00 +0200

    Removed LoginRequiredMixin from the homepage

--- reservation_tool_base_folder/views.py
+++ reservation_tool_base_folder/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
-class HomepageView(LoginRequiredMixin, TemplateView):
+class HomepageView(TemplateView):
     template_name = 'home.html'
 
 
