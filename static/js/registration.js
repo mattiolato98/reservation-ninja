00e1b4af24d539c971384734a006285f4a5c8cd3
gabrymattioli <gabrymattioli@gmail.com>
2021-10-17 12:50:07 +0200

    Undo move js

--- static/js/registration.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $("#submit-signing-button").prop("disabled", true);
-
-    $("#id_privacy_and_cookie_policy_acceptance").on('change', function () {
-        $("#submit-signing-button").prop("disabled", !$(this).is(":checked"));
-    });
-});
