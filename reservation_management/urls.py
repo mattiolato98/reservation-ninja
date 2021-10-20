c7928edd811bb7fea5321e3db938d36cfc995fa4
gabrymattioli <gabrymattioli@gmail.com>
2021-10-20 14:42:46 +0200

    Commented lesson-delete url

--- reservation_management/urls.py
+++ reservation_management/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('lesson/list', views.LessonListView.as_view(), name='lesson-list'),
     path('lesson/<int:pk>/detail', views.LessonDetailView.as_view(), name='lesson-detail'),
     path('lesson/<int:pk>/update', views.LessonUpdateView.as_view(), name='lesson-update'),
-    path('lesson/<int:pk>/delete', views.LessonDeleteView.as_view(), name='lesson-delete'),
+    # path('lesson/<int:pk>/delete', views.LessonDeleteView.as_view(), name='lesson-delete'),
     path('log-list', views.LogListView.as_view(), name='log-list'),
 ]
