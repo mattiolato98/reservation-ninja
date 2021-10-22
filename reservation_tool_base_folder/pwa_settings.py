7addc3e5ccacb0cadc4c3f6088a92a860e700dd2
gabrymattioli <gabrymattioli@gmail.com>
2021-10-22 17:09:47 +0200

    Added pwa settings

---
+++ reservation_tool_base_folder/pwa_settings.py
@@ -0,0 +1,30 @@
+PWA_APP_NAME = 'Reservation Ninja'
+PWA_APP_DESCRIPTION = "Reservation Ninja"
+PWA_APP_THEME_COLOR = '#ff5964'
+PWA_APP_BACKGROUND_COLOR = '#ff5964'
+PWA_APP_DISPLAY = 'standalone'
+PWA_APP_SCOPE = '/'
+PWA_APP_ORIENTATION = 'portrait'
+PWA_APP_START_URL = '/'
+PWA_APP_STATUS_BAR_COLOR = 'default'
+PWA_APP_ICONS = [
+    {
+        'src': '/static/img/ninja.png',
+        'sizes': '512x512'
+    }
+]
+PWA_APP_ICONS_APPLE = [
+    {
+        'src': '/static/img/ninja.png',
+        'sizes': '512x512'
+    }
+]
+PWA_APP_SPLASH_SCREEN = [
+    {
+        'src': '/static/img/ninja.png',
+        'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+    }
+]
+PWA_APP_DIR = 'ltr'
+PWA_APP_LANG = 'en-US'
+PWA_APP_DEBUG_MODE = False
