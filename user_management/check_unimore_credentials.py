e9cc09c6ecdfb90cddd8ec96956bb10db5d9b42b
Filippo Fontana <filippofontana1998@gmail.com>
2021-11-01 13:24:35 +0100

    Added performance measures in check unimore credentials

--- user_management/check_unimore_credentials.py
+++ user_management/check_unimore_credentials.py
@@ -3,6 +3,7 @@ from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import NoSuchElementException
 
 from webbot import Browser
+from time import time as measure_time
 
 
 LOGIN_URL = "https://in.unimore.it"
@@ -10,6 +11,7 @@ TIME_INTERVAL = 5
 
 
 def check_unimore_credentials(username, password):
+    start = measure_time()
     # Allows to run Firefox on a system with no display
     options = Options()
     options.headless = True
@@ -19,6 +21,9 @@ def check_unimore_credentials(username, password):
 
     driver.get(LOGIN_URL)
 
+    end = measure_time()
+    print(f'Elpased time (create driver): {end - start}')
+    start = measure_time()
     try:
         driver.find_element_by_id("username").send_keys(username)
         driver.find_element_by_id("password").send_keys(password)
@@ -28,12 +33,17 @@ def check_unimore_credentials(username, password):
         driver.delete_all_cookies()
         driver.quit()
         return False
+    end = measure_time()
+    print(f'Elpased time (compile form): {end - start}')
+    start = measure_time()
     try:
         driver.find_element_by_xpath("//a[contains(text(), 'Esci')]").click()
     except NoSuchElementException:
         driver.delete_all_cookies()
         driver.quit()
         return False
+    end = measure_time()
+    print(f'Elpased time (search for Exit): {end - start}')
     driver.delete_all_cookies()
     driver.quit()
 
@@ -60,3 +70,9 @@ def webbot_check(username, passwd):
 
     return check_strings_equality(CORRECT_URL, test_url)
 
+if __name__ == "__main__":
+    start = measure_time()
+    # check_unimore_credentials("username", "password")
+    webbot_check("username", "password")
+    end = measure_time()
+    print(f'Elpased time: {end - start}')
