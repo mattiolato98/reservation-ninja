9d53424d8c86c598de71deafeb89683bbee01277
Filippo Fontana <filippofontana1998@gmail.com>
2021-11-01 13:12:52 +0100

    Added webbot implementation of check unimore credentials

--- user_management/check_unimore_credentials.py
+++ user_management/check_unimore_credentials.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import NoSuchElementException
 
+from webbot import Browser
+
+
 LOGIN_URL = "https://in.unimore.it"
 TIME_INTERVAL = 5
 
@@ -25,15 +28,35 @@ def check_unimore_credentials(username, password):
         driver.delete_all_cookies()
         driver.quit()
         return False
-
     try:
         driver.find_element_by_xpath("//a[contains(text(), 'Esci')]").click()
     except NoSuchElementException:
         driver.delete_all_cookies()
         driver.quit()
         return False
-
     driver.delete_all_cookies()
     driver.quit()
 
     return True
+
+
+def check_strings_equality(first, second):
+    return len(first) == len(second) and hash(first) == hash(second) and first == second    
+
+
+def webbot_check(username, passwd):
+    CORRECT_URL = "https://in.unimore.it/intra/"
+    start = measure_time()
+    driver = Browser(showWindow=False)
+    end = measure_time()
+    print(f'Elpased time (create driver): {end - start}')
+
+    driver.go_to(LOGIN_URL)
+    driver.type(username, id="username")
+    driver.type(passwd, id="password")
+    driver.click(tag="button", classname="form-button")
+    test_url = driver.get_current_url()
+    driver.quit()
+
+    return check_strings_equality(CORRECT_URL, test_url)
+
