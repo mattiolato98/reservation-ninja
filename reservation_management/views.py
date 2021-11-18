5821b295cfbb49f875801c20b464ef8632fcd846
Filippo Fontana <filippofontana1998@gmail.com>
2021-11-18 10:38:13 +0100

    Implemented the timetable view

--- reservation_management/views.py
+++ reservation_management/views.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 from collections import defaultdict
 
+import math
+
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect, JsonResponse
@@ -118,64 +120,89 @@ class FeedbackListView(ListView):
 
 
 class LessonTimetableView(LoginRequiredMixin, TemplateView):
+    """
+    View to display the timetable of the current user.
+    """
     template_name = "reservation_management/lesson_timetable.html"
 
     def get_context_data(self, **kwargs):
         context = super(LessonTimetableView, self).get_context_data(**kwargs)
 
         lessons = self.request.user.lessons.all()
+        min_time = min(lesson.get_approximated_start_time for lesson in lessons)
+        max_time = max(lesson.get_approximated_end_time for lesson in lessons)
+        sorted_times = set()
 
-        min_time = min(lesson.start_time for lesson in lessons)
-        max_time = max(lesson.end_time for lesson in lessons)
+        for lesson in lessons:
+            sorted_times.add(lesson.get_approximated_start_time)
+            sorted_times.add(lesson.get_approximated_end_time)
+        sorted_times = list(sorted(sorted_times))  # in order to access elements by index
 
-        if any(lesson.start_time.minute != 0 or lesson.end_time.minute != 0 for lesson in lessons):
-            frequency = 30
-        else:
-            frequency = 60
+        # retrieving all possible deltas between each lesson
+        time_deltas = {
+            dt.datetime.combine(
+                dt.date.min,
+                sorted_times[idx + 1]
+            ) - dt.datetime.combine(
+                dt.datetime.min,
+                sorted_times[idx]
+            )
+            for idx in range(len(sorted_times))
+            if idx < len(sorted_times) - 1
+        }
+        time_deltas = {
+            int((delta.seconds % 3600) / 60)  # distribute the durations in an hour and convert seconds in minutes
+            for delta in time_deltas
+            if delta.seconds % 3600 != 0
+        }
+        time_interval = math.gcd(*time_deltas, 60)  # this is the time interval of the timetable
 
         time_list = []
-        items = 0
-        time = min_time
-        while time <= max_time:
-            time_list.append(time)
-            time = (dt.datetime.combine(dt.date(1, 1, 1), time) + dt.timedelta(minutes=frequency)).time()
-            items += 1
-
+        current_time_slot = min_time
+        while current_time_slot < max_time:
+            time_list.append(current_time_slot)
+            current_time_slot = (dt.datetime.combine(dt.date.min, current_time_slot)
+                                 + dt.timedelta(minutes=time_interval)).time()
+        # this index will be used in a pandas DataFrame
         index = [
             f"{time.strftime('%H:%M')} - "
-            f"{(dt.datetime.combine(dt.date(1, 1, 1), time) + dt.timedelta(minutes=frequency)).time().strftime('%H:%M')}"
+            f"""{(dt.datetime.combine(dt.date.min, time) 
+                 + dt.timedelta(minutes=time_interval)).time().strftime('%H:%M')}"""
             for time in time_list
         ]
-
-        lessons_grouped_by_day = {
+        # this empty dictionary has to be instantiated early because a user
+        # could not have lessons every day
+        lessons_by_day = {
             0: [],
             1: [],
             2: [],
             3: [],
             4: [],
         }
+        for lesson in lessons:  # the lessons are already ordered
+            lessons_by_day[lesson.day].append(lesson)
 
-        for lesson in lessons:
-            lessons_grouped_by_day[lesson.day].append(lesson)
-
-        data = defaultdict(list)
-        for day, lessons in lessons_grouped_by_day.items():
+        data = defaultdict(list)  # the content of the pandas DataFrame
+        for day, lessons in lessons_by_day.items():  # creating the actual timetable
             lesson = lessons.pop(0) if len(lessons) > 0 else None
-            time = min_time
-            while time <= max_time:
-                next_time = (dt.datetime.combine(dt.date(1, 1, 1), time) + dt.timedelta(minutes=frequency)).time()
+            current_time_slot = min_time
+            while current_time_slot < max_time:
+                next_time_slot = (
+                        dt.datetime.combine(dt.date.min, current_time_slot)
+                        + dt.timedelta(minutes=time_interval)
+                ).time()
 
                 if lesson is not None \
-                        and lesson.start_time <= time < lesson.end_time \
-                        and lesson.end_time >= next_time > lesson.start_time:
-                    data[day].append(lesson.classroom.name)
+                        and lesson.get_approximated_start_time <= current_time_slot \
+                        and lesson.get_approximated_end_time >= next_time_slot:
+                    data[day].append(lesson)
                 else:
-                    data[day].append('')
+                    data[day].append('')  # if there is no lesson, we have to fill the time slot anyway
 
-                if lesson is not None and lesson.end_time <= next_time:
+                if lesson is not None and lesson.end_time <= next_time_slot:
                     lesson = lessons.pop(0) if len(lessons) > 0 else None
 
-                time = next_time
+                current_time_slot = next_time_slot
 
         context['df'] = pd.DataFrame(data, index=index)
 
