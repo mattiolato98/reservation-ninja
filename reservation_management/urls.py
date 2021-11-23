947d33d2c98522f4a4e888700dd13f627ebaa2f7
Filippo Fontana <filippofontana1998@gmail.com>
2021-11-23 14:43:40 +0100

    Added lesson-overlap-error url

--- reservation_management/urls.py
+++ reservation_management/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('lesson/<int:pk>/update', views.LessonUpdateView.as_view(), name='lesson-update'),
     path('lesson/<int:pk>/delete', views.LessonDeleteView.as_view(), name='lesson-delete'),
     path('lesson-timetable', views.LessonTimetableView.as_view(), name='lesson-timetable'),
+    path('lesson-overlap-error', views.LessonOverlapErrorView.as_view(), name='lesson-overlap-error'),
     path('whats-new-confirm', views.ajax_whats_new_confirm, name='whats-new-confirm'),
     path('instagram-confirm', views.ajax_instagram_confirm, name='instagram-confirm'),
 ]
